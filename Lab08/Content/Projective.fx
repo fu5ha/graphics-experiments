3fa389713322947585f5c8f16cf5f80bd836f03a
Gray Olson <gray@grayolson.com>
2019-03-09 20:02:50 -0700

    lab8

---
+++ Lab08/Content/Projective.fx
@@ -0,0 +1,70 @@
+﻿
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+float4x4 WorldInverseTranspose;
+float4x4 LightViewMatrix;
+float4x4 LightProjectionMatrix;
+float3 CameraPosition;
+float3 LightPosition;
+
+texture Texture;
+sampler TextureSampler = sampler_state
+{
+	Texture = <Texture>;
+	MinFilter = none;
+	MagFilter = none;
+	MipFilter = none;
+	AddressU = border;
+	AddressV = border;
+};
+
+struct VertexShaderInput
+{
+	float4 Position : POSITION0;
+	float4 Normal : NORMAL0;
+	float2 TexCoord : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : POSITION0;
+	float3 Normal	: TEXCOORD0;
+	float2 TexCoord : TEXCOORD1;
+	float3 WorldPosition : TEXCOORD2;
+};
+
+VertexShaderOutput VSFunction(VertexShaderInput input)
+{
+	VertexShaderOutput output;
+	float4 worldPos = mul(input.Position, World);
+	float4 viewPosition = mul(worldPos, View);
+	output.Position = mul(viewPosition, Projection);
+	output.WorldPosition = worldPos.xyz;
+	output.Normal = normalize(mul(input.Normal, WorldInverseTranspose).xyz);  
+	output.TexCoord = input.TexCoord;
+	return output;
+}
+
+float4 PSFunction(VertexShaderOutput input) : COLOR0
+{
+	float4 projTexCoord = mul(mul(float4(input.WorldPosition, 1.0), LightViewMatrix), LightProjectionMatrix);
+	projTexCoord = projTexCoord / projTexCoord.w;
+	projTexCoord.y *= -1.0;
+	projTexCoord.xy = projTexCoord.xy * 0.5 + 0.5;
+	float depth = 1.0 - projTexCoord.z;
+	float4 color = (depth > 0.0) ? float4(tex2D(TextureSampler, projTexCoord.xy).xyz, 1) : 0;
+	if (color.x == 0 && color.y == 1 && color.z == 1) color.xyz = float3(0, 0, 0);
+	float3 N = normalize(input.Normal);
+	float3 L = normalize(LightPosition - input.WorldPosition);
+	if (dot(L, N) < 0) color = 0;
+	return color;
+}
+technique Technique1
+{
+	pass Pass1
+	{
+		VertexShader = compile vs_4_0 VSFunction();
+		PixelShader = compile ps_4_0 PSFunction();
+	}
+}
---
+++ Lab08/Lab08.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lab08
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Lab08 : Game
+    {
+        SpriteFont font;
+        Effect effect;
+        Effect projective;
+        Effect depthmap;
+        Matrix world = Matrix.Identity;
+        Matrix view = Matrix.CreateLookAt(
+            new Vector3(0, 0, 20),
+            new Vector3(0, 0, 0),
+            Vector3.UnitY);
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+            MathHelper.ToRadians(45),
+            800f / 600f,
+            0.1f,
+            100f);
+        Vector3 cameraPosition, cameraTarget, lightPosition;
+        Matrix lightView;
+        Matrix lightProjection = Matrix.CreatePerspectiveFieldOfView(
+            MathHelper.PiOver2, 1f, 1f, 50f);
+        float angle = 0;
+        float angle2 = 0;
+        float angleL = 0;
+        float angleL2 = 0;
+        float distance = 20;
+        MouseState preMouse;
+        Model model;
+        Texture2D texture;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Lab08()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rs;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //font = Content.Load<SpriteFont>("");
+            model = Content.Load<Model>("Plane");
+            projective = Content.Load<Effect>("Projective");
+            depthmap = Content.Load<Effect>("DepthMap");
+            effect = projective;
+
+            texture = Content.Load<Texture2D>("nvlobby_new_negz");
+            projective.Parameters["Texture"].SetValue(texture);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // ************ TEMPLATE ************ //
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) angleL += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) angleL -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) angleL2 += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) angleL2 -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { angle = angle2 = angleL = angleL2 = 0; distance = 30; cameraTarget = Vector3.Zero; }
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle -= (Mouse.GetState().X - preMouse.X) / 100f;
+                angle2 += (Mouse.GetState().Y - preMouse.Y) / 100f;
+            }
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - preMouse.X) / 100f;
+            }
+
+            if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - preMouse.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - preMouse.Y) / 10f;
+            }
+            preMouse = Mouse.GetState();
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance),
+                Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle) * Matrix.CreateTranslation(cameraTarget));
+            view = Matrix.CreateLookAt(
+                cameraPosition,
+                cameraTarget,
+                Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)));
+            lightPosition = Vector3.Transform(
+                new Vector3(0, 0, 10),
+                Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL));
+            lightView = Matrix.CreateLookAt(
+                lightPosition,
+                Vector3.Zero,
+                Vector3.Transform(
+                    Vector3.UnitY,
+                    Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL)));
+            // ******************************//
+
+            if (Keyboard.GetState().IsKeyDown(Keys.F1)) effect = projective;
+            if (Keyboard.GetState().IsKeyDown(Keys.F2)) effect = depthmap;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            if (effect == projective)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+            } else
+            {
+                GraphicsDevice.Clear(Color.Black);
+            }
+
+            // ************ TEMPLATE ************ //
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
+
+            effect.CurrentTechnique = effect.Techniques[0];
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                foreach (ModelMesh mesh in model.Meshes)
+                {
+                    foreach (ModelMeshPart part in mesh.MeshParts)
+                    {
+
+                        effect.Parameters["World"].SetValue(mesh.ParentBone.Transform);
+                        effect.Parameters["View"].SetValue(view);
+                        effect.Parameters["Projection"].SetValue(projection);
+                        Matrix worldInverseTransposeMatrix = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform));
+                        effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTransposeMatrix);
+                        effect.Parameters["LightViewMatrix"].SetValue(lightView);
+                        effect.Parameters["LightProjectionMatrix"].SetValue(lightProjection);
+                        effect.Parameters["LightPosition"].SetValue(lightPosition);
+                        effect.Parameters["CameraPosition"].SetValue(cameraPosition);
+                        pass.Apply();
+                        GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                        GraphicsDevice.Indices = part.IndexBuffer;
+                        GraphicsDevice.DrawIndexedPrimitives(
+                            PrimitiveType.TriangleList,
+                            part.VertexOffset,
+                            part.StartIndex,
+                            part.PrimitiveCount);
+                    }
+                }
+            }
+            // ************************************* //
+
+            base.Draw(gameTime);
+        }
+    }
+}
